1e5130d5ef791fcd06525829b2593ed1ebeacb53
Raynoonan <raynoonan03@gmail.com>
2025-01-24 22:15:48 -0500

    Update data.sql

--- eclipse-workspace/CrochetStore/src/main/resources/data.sql
+++ eclipse-workspace/CrochetStore/src/main/resources/data.sql
@@ -1,24 +1,12 @@
-INSERT INTO store (store_name, city, state, zip) VALUES ('Crochet Store', 'NYC', 'NY', '0093' );
+INSERT INTO store (store_name, city, state, zip) VALUES ('Crochet Store', 'BlueBank', 'NJ', '07730' );
 
-INSERT INTO supplies (store_id, material) VALUES (1, 'wool yarn');
+INSERT INTO supplies (store_id, material, type, color, size_or_weight) VALUES (1, 'yarn', 'wool', 'blue', 5.5);
+INSERT INTO supplies (store_id, material, type, color, size_or_weight) VALUES (1, 'yarn', 'alpaca', 'beige', 6.5);
+INSERT INTO supplies (store_id, material, type, color, size_or_weight) VALUES (1, 'yarn', 'cotton', 'turquoise', 3.5);
+INSERT INTO supplies (store_id, material, type, color, size_or_weight) VALUES (1, 'metal', 'hook', 'silver', 3.5);
+INSERT INTO supplies (store_id, material, type, color, size_or_weight) VALUES (1, 'metal', 'needle', 'silver', 9.0 );
+INSERT INTO supplies (store_id, material, type, color, size_or_weight) VALUES (1, 'safety eyes', 'plastic', 'black', 2.5);
+INSERT INTO supplies (store_id, material, type, color, size_or_weight) VALUES (1, 'plastic', 'stitch marker', 'red', null);
 
-INSERT INTO supplies (store_id, type) VALUES (2, 'hook');
-INSERT INTO supplies (store_id, type) VALUES (3,'stitch marker');
-INSERT INTO supplies (store_id, type) VALUES (4,'needle');
-INSERT INTO supplies (store_id,type) VALUES (5,'safety eyes');
-INSERT INTO supplies (store_id,material) VALUES (6,'cotton yarn');
-INSERT INTO supplies (store_id,material) VALUES (7,'alpaca yarn');
-
-INSERT INTO supplies (store_id, color) VALUES (1, 'blue');
-INSERT INTO supplies (store_id, color) VALUES (2, 'purple');
-INSERT INTO supplies (store_id, color) VALUES (3, 'turquoise');
-INSERT INTO supplies (store_id, color) VALUES (4, 'red');
-INSERT INTO supplies (store_id, color) VALUES (5, 'white');
-INSERT INTO supplies (store_id, color) VALUES (6, 'beige');
-INSERT INTO supplies (store_id, color) VALUES (7, 'brown');
-
-INSERT INTO supplies (store_id, size_or_weight) VALUES (1, 5.5);
-INSERT INTO supplies (store_id, size_or_weight) VALUES (2, 6.5);
-INSERT INTO supplies (store_id, size_or_weight) VALUES (3, 8.5);
 
 
